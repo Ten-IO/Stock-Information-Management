1ef0286ff85849f83814c38667c1d143774970ff
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-06-17 23:57:26 +0700

    04-re-structure

--- test/item-test.cpp
+++ test/item-test.cpp
@@ -4,6 +4,7 @@
 int main() {
     Item prod1 = Item{01,22,"string 1","category", 1.1}; 
     Item *prod2 = new Item{02,23, "string 2", "cat2", -1.2};
+    // c-20
     Item prod3 = {
     .id = 1,
     .units = 22,
@@ -11,6 +12,9 @@ int main() {
     .category = "Gadgets",
     .unitPrice = 10.0,
 };
+    // double ptr = &prod3.unitPrice; // pointer is one data type for addr
 
-    std::cout <<"1- "<< prod1.id <<"\n2-"<< prod2->unitPrice << "\n3- " << prod3.units << "\n4- " << prod1.category;  
+    std::cout <<"1- "<< prod1.id <<"\n2-"<< prod1.unitPrice << "\n3- " << prod1.units << "\n4- " << prod1.category;  
+    std::cout <<"1- "<< prod2->id <<"\n2-"<< prod2->unitPrice << "\n3- " << (*prod2).units << "\n4- " << (*prod2).category;  
+    std::cout <<"1- "<< prod3.id <<"\n2-"<< (&prod3.unitPrice) << "\n3- " << prod3.units << "\n4- " << prod3.category;  
 }
