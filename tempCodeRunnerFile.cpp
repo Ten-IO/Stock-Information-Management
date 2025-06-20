3c4c9738459a8240df4ce5cfe27bad01092cb436
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-06-21 00:02:34 +0700

    13-mini interface, yet to define

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+void interface_create(List* ls, Input data){
+
+//     cout << "-----------Create-----------"<<endl;
+//     cout << "1. Enter the product type: ";
+//     cin >> data.productType;
+//     cout << "2. Enter the product code: ";
+//     cin >> data.productCode;
+//     cout << "3. Enter cost per unit: ";
+//     cin >> data.costUnit;
+//     cout << "4. Enter stock level: ";
+//     cin >> data.stockLevel;
+//     cout << "5. Enter low stock level: ";
+//     cin >> data.lowLevel;
+}
