93c0bb470e0756848e15d4387b5d80c27f0f17f5
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-06-25 22:18:25 +0700

    16-test on sort, check state, !BEWARE prevent bad-alloc (n-1) - arr start 0

--- test/sort-test.cpp
+++ test/sort-test.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
+#include <stock.h>
+#include <creator.h>
+#include <sorter.h>
+#include <displayManager.h>
+#include <reader.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    List *s = initList();
+    Item i1 = Item{101, 20, "GoPro", "Camera", 400};
+    Item i2 = Item{103, 10, "VXE R1", "Mouse", 18};
+    Item i3 = Item{102, 110, "Window AK", "License Box", 5};
+    Item i4 = Item{104, 50, "", "Keyboard", 400};
+    Item i5 = Item{105, 220, "ESP32", "Micro-Controller", 400};
 
+    addEnd(s, i1);
+    addEnd(s, i2);
+    addEnd(s, i3);
+    addEnd(s, i4);
+    addEnd(s, i5);
+    addEnd(s, i3);
+    addEnd(s, i2);
+    addEnd(s, i4);
+    addEnd(s, i1);
+    addEnd(s, i3);
+    addEnd(s, i2);
+    addEnd(s, i3);
+    addEnd(s, i5);
+    addEnd(s, i2);
+    addEnd(s, i1);
+    addEnd(s, i3);
+    addEnd(s, i4);
+    addEnd(s, i1);
+    addEnd(s, i5);
+    addEnd(s, i4);
+    addEnd(s, i2);
+    addEnd(s, i1);
+    addEnd(s, i3);
+    addEnd(s, i4);
+    addEnd(s, i2);
+    addEnd(s, i3);
+    addEnd(s, i2);
+    addEnd(s, i2);
+
+    std::cout << "+ Initial State:\n";
+    display(s);
+    std::cout << "\n%list: " << s->n << std::endl;
+
+    mergeSortList(&s, 0, s->n - 1); // Check not to be outbound - If we put "n" but in sort put [size+1] work, but arr[0] empty
+    std::cout << "+ Sort State:\n";
+
+    display(s);
 }
