537dfbf703e82ed1a05b41f0a78976470db95d86
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-06-25 22:16:33 +0700

    16-correction on sort, change Arr-Stock to Arr-Item, save mem

--- lib/sorter.h
+++ lib/sorter.h
@@ -1,6 +1,37 @@
 #ifndef STOCK_H
 #define STOCK_H
 #include <creator.h>
+#include <new>
+#include <iostream>
+#include <reader.h>
+
+/**
+ * @brief factory assister : turn array to static for quicker sort
+ * @param List* desired sort list
+ * @return array:Stock
+ */
+Item *ListToArr(List *ls, int size)
+{
+    Item *arr = new Item[size];
+    if (size == 0)
+        return nullptr;
+    Stock *current = ls->head;
+    for (int i = 0; i < size && current != nullptr; i++)
+    {
+        arr[i] = current->item;
+        current = current->next;
+    }
+    return arr;
+}
+
+List *ArrToList(Item *arr, int size)
+{
+    List *ls = initList();
+    for (int i = 0; i < size; i++)
+        addEnd(ls, arr[i]);
+
+    return ls;
+}
 
 /**
  * @brief divider, comparer, and recontructor for array
@@ -10,11 +41,11 @@
  * @param right hold end pos
  * @return void
  */
-void divideMerge(Stock arr[], int left, int mid, int right)
+void divideMerge(Item arr[], int left, int mid, int right)
 {
     int par1 = mid - left + 1, par2 = right - mid;
 
-    Stock *leftSide = new Stock[par1], *rightSide = new Stock[par2];
+    Item *leftSide = new Item[par1], *rightSide = new Item[par2];
 
     for (int i = 0; i < par1; i++)
         leftSide[i] = arr[left + i];
@@ -23,7 +54,7 @@ void divideMerge(Stock arr[], int left, int mid, int right)
     int i = 0, j = 0, k = left;
     while (i < par1 && j < par2)
     {
-        if (leftSide[i].item.id <= rightSide[j].item.id)
+        if (leftSide[i].id <= rightSide[j].id)
         {
             arr[k] = leftSide[i];
             i++;
@@ -52,74 +83,49 @@ void divideMerge(Stock arr[], int left, int mid, int right)
 }
 
 /**
+ * @brief factory assister : turn array to static for quicker sort
+ * @param List* desired sort list
+ * @return array:Stock
+ */
+void mergeSortArr(Item *arr, int pos1, int pos2)
+{
+    if (pos1 < pos2)
+    {
+        int mid = pos1 + (pos2 - pos1) / 2;
+
+        mergeSortArr(arr, pos1, mid);
+        mergeSortArr(arr, mid + 1, pos2);
+
+        divideMerge(arr, pos1, mid, pos2);
+    }
+}
+
+/**
  * @brief factory for sort, make changes to LinkedList
  * @param List* point linkedlist
  * @param pos1 starting point of arrangement
  * @param pos2 end point of arrangement
  * @return void
  */
-void mergeSort(List *ls, int pos1, int pos2)
+void mergeSortList(List **ls, int pos1, int pos2)
 {
-    int size = ls->n;
-    Stock *arr = ListToArr(ls, size);
+
+    int size = (*ls)->n;
+    Item *arr;
+
+    arr = ListToArr(*ls, size);
+
     if (pos1 < pos2)
     {
         int mid = pos1 + (pos2 - pos1) / 2;
 
-        mergeSort(arr, pos1, mid);
-        mergeSort(arr, mid + 1, pos2);
+        mergeSortArr(arr, pos1, mid);
+        mergeSortArr(arr, mid + 1, pos2);
 
         divideMerge(arr, pos1, mid, pos2);
     }
-    ls = ArrToList(arr, size);
+    *ls = ArrToList(arr, size);
     delete[] arr;
-}
-
-/**
- * @brief factory assister : turn array to static for quicker sort
- * @param List* desired sort list
- * @return array:Stock
- */
-void mergeSort(Stock *arr, int pos1, int pos2)
-{
-    if (pos1 < pos2)
-    {
-        int mid = pos1 + (pos2 - pos1) / 2;
-
-        mergeSort(arr, pos1, mid);
-        mergeSort(arr, mid + 1, pos2);
-
-        divideMerge(arr, pos1, mid, pos2);
-    }
-}
-
-/**
- * @brief factory assister : turn array to static for quicker sort
- * @param List* desired sort list
- * @return array:Stock
- */
-Stock *ListToArr(List *ls, int size)
-{
-    int size = ls->n;
-    Stock *arr = new Stock[size];
-    if (size == 0)
-        return nullptr;
-    Stock *current = ls->head;
-    for (int i = 0; i < size && current != nullptr; i++)
-    {
-        arr[i].item = current->item;
-        current = current->next;
-    }
-    return arr;
-}
-
-List *ArrToList(Stock *arr, int size)
-{
-    List *ls = initList();
-    for (int i = 0; i < size; i++)
-        addEnd(ls, arr[i].item);
-
-    return ls;
 }
 
 #endif
