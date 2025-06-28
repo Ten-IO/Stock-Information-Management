ec41885f9eb84c6b1e9a3ffbcba2f9bebea6e6a9
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-06-28 12:13:35 +0700

    18-adding normal art, take care encoding

--- test/displayManager.cpp
+++ test/displayManager.cpp
@@ -3,41 +3,49 @@
 #include <thread>
 #include <atomic>
 #include <chrono>
+#include <cstdlib>
 
 std::atomic<bool> stopFlag(false);
 
-int getConsoleWidth() {
+int getConsoleWidth()
+{
     CONSOLE_SCREEN_BUFFER_INFO csbi;
-    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+    {
         return csbi.srWindow.Right - csbi.srWindow.Left + 1;
     }
     return -1;
 }
 
-void trackConsoleWidthChanges() {
+void trackConsoleWidthChanges()
+{
     int lastWidth = getConsoleWidth();
 
-    std::cout << "Initial console width: " << lastWidth << std::endl;
+    std::cout << "init console width: " << lastWidth << std::endl;
 
-    while (!stopFlag.load()) {
+    while (!stopFlag.load())
+    {
         int currentWidth = getConsoleWidth();
-        if (currentWidth != lastWidth) {
-            std::cout << "Console width changed: " << currentWidth << std::endl;
+        if (currentWidth != lastWidth)
+        {
+            std::cout << "+ width changed: " << currentWidth << std::endl;
             lastWidth = currentWidth;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));  // poll every 200ms
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
     std::cout << "Stopped tracking console width changes.\n";
 }
 
-int main() {
+int main()
+{
     std::thread tracker(trackConsoleWidthChanges);
 
-    std::this_thread::sleep_for(std::chrono::seconds(20));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     stopFlag = true;
 
     tracker.join();
     std::cout << "Tracker thread finished.\n";
+    system("powershell -Command \"$host.ui.rawui.windowsize\"");
 
     return 0;
 }
