8d8619a30c36b3e6d512caccda362a13c7b9d4fa
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-07-16 21:59:38 +0700

    30 - add: prototype of duplicate and checking incorrect input, prototype for updating item; rename: label.h and full return on string

--- lib/modifier.h
+++ lib/modifier.h
@@ -4,34 +4,9 @@
 #include <stock.h>
 #include <iostream>
 
-/**
- * @brief look for old info via `id` then update
- * @param ls point to list
- * @param oldData not wanted
- * @param newData replace
- * @return void
- */
-void modifyViaID(List *, Item, Item);
 
 void modifyViaRatio(List *, Item, Item);
 
-    void modifyViaID(List *ls, Item oldData, Item newData)
-{
-    Stock *s = ls->head;
-    int isFound = 0;
-    while (s != nullptr)
-    {
-        if (s->item.id == oldData.id)
-        {
-            s->item = newData;
-            isFound += 1;
-        }
-    }
-    s = s->next;
-    if (isFound != 0)
-        std::cout << "data updated\n";
-    else
-        std::cout << "data unchange\n";
-}
+
 
 #endif
---
+++ lib/prototypecreator.h
@@ -0,0 +1,10 @@
+#ifndef CREATOR_H
+#define CREATOR_H
+
+/**
+ * @brief Check duplication via name and minimize information through that.
+ * @param 
+ * @return void
+ */
+void duplicateRegister();
+#endif
