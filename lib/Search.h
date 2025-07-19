a9efdd9531000522834780be4042a8b1cc131f2b
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-07-19 23:57:10 +0700

    32 - add: search levenshtein, benchmark re-implementation

---
+++ lib/Search.h
@@ -0,0 +1,55 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+#include <string>
+#include <iostream>
+#include "StockList.h"
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int levenshtein_distance(std::string usrTxt, std::string dbTxt, bool verbose)
+{
+    int m = usrTxt.length(), n = dbTxt.length(), cost = 0;
+    int d[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++)
+        d[i][0] = i;
+    for (int i = 0; i <= n; i++)
+        d[0][i] = i;
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cost = usrTxt.at(i - 1) != dbTxt.at(j - 1) ? 1 : 0;
+            d[i][j] = min(d[i - 1][j - 1] + cost, d[i][j - 1] + 1);
+            d[i][j] = min(d[i][j], d[i - 1][j] + 1);
+        }
+    }
+    if (verbose)
+    {
+        for (int x = 0; x < m; x++)
+        {
+            std::cout << '\n';
+            for (int y = 0; y < n; y++)
+                std::cout << d[x][y] << " ";
+        }
+        std::cout << '\n';
+    }
+
+    return d[m][n];
+}
+
+std::string levenshtein_search(std::string input, List *ls)
+{
+    Stock *s = new Stock;
+    s->next = ls->head;
+    for (int i = 0; i < ls->n; i++)
+    {
+        levenshtein_distance(input, s->item.name, 0);
+        s->next;
+    }
+}
+#endif
--- lib/benchmark.h
+++ lib/benchmark.h
@@ -1,14 +1,28 @@
 #ifndef BENCHMARK_H
 #define BENCHMARK_H
 #include <chrono>
-
+#include <iostream>
 class Timer
 {
 public:
     std::chrono::time_point<std::chrono::high_resolution_clock> m_start, m_stop;
-    Timer();
-    ~Timer();
-    void flickStart();
-    void flickStop();
+    Timer()
+    {
+        flickStart();
+    }
+    ~Timer()
+    {
+        flickStop();
+    }
+    void flickStart()
+    {
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+    void flickStop()
+    {
+        m_stop = std::chrono::high_resolution_clock::now();
+        std::chrono::nanoseconds elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(m_stop - m_start);
+        std::cout << "\nTimed: " << elapsed.count() / 1e6 << "ms <-> " << elapsed.count() << "ns\n";
+    }
 };
 #endif
