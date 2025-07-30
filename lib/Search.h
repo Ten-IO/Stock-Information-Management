2453d25abee00a4ec706a03f63be2d4cb1096f2b
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-07-30 16:05:29 +0700

    update search documentation

--- lib/Search.h
+++ lib/Search.h
@@ -5,7 +5,40 @@
 #include "StockList.h"
 #include "DisplayManager.h"
 
+/**
+ * @brief UI cleaner for live fuzzy search
+ * @param row accept row to move up and clean
+ * @return void
+ */
 void clearSearchTable(const int &);
+
+/**
+ * @brief minimal numeric finder
+ * @param a first number, @param b second number
+ * @retval smallest value
+ */
+int min(int a, int b);
+
+/**
+ * @brief List - Algorithm connector, allow using string and List database to look up for max amount of closeness text
+ * @param input value to search @param ls Linked List to search @param numLook amount of return finds 
+ * @retval List of finding value from LinkedList
+ */
+List *levenshtein_search(std::string input, List *ls, const int &numLook);
+
+/**
+ * @brief approximate search algorithm via editing cost: using compare substitution, replacement, deletion
+ * @param usrText target @param dbText source @param verbose show its matrix value
+ * @retval cost distance between the two text
+ */
+int levenshtein_distance(std::string usrTxt, std::string dbTxt, bool verbose);
+
+/**
+ * @brief a display function construct to update with each key input
+ * @param ls LinkedList to search in @param maxLook size to search for
+ */
+void fuzzyShow(List *ls, const int &maxLook);
+
 int min(int a, int b)
 {
     return a < b ? a : b;
