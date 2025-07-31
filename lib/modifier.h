9634acd8480d1fb8843bd1b9927c772f9dcbf7e8
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-07-31 12:16:57 +0700

    add: feature exporting name

--- lib/modifier.h
+++ lib/modifier.h
@@ -4,8 +4,6 @@
 #include "StockList.h"
 #include "DisplayManager.h"
 
-void modifyViaRatio(List *, Item, Item);
-
 bool exportByID(List *ls, int &id, std::string &nameOut, int &qtyOut)
 {
 
@@ -74,5 +72,76 @@ bool exportByID(List *ls, int &id, std::string &nameOut, int &qtyOut)
     qtyOut = exportQty;
 
     std::cout << "\n[-] Exported " << exportQty << " unit(s) of item ID " << id << ".\n";
+    return true;
+}
+
+bool exportByName(List *ls, int &id, std::string &name, int &qtyOut)
+{
+
+    if (ls == nullptr || ls->head == nullptr)
+    {
+        std::cout << "\n[!] No stock available.\n";
+        return false;
+    }
+
+    Stock *current = ls->head;
+    Stock *prev = nullptr;
+
+    while (current != nullptr && current->item.name != name)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == nullptr)
+    {
+        std::cout << "\n[!] Item with name " << name << " not found.\n";
+        return false;
+    }
+
+    int availableUnits = current->item.units;
+    std::cout << "\n[=] Available units: " << availableUnits << "\n";
+    inputBox(1);
+    int exportQty = readInt("Quantity to export: ");
+
+    if (exportQty <= 0)
+    {
+        std::cout << "\n[!] Invalid export quantity.\n";
+        return false;
+    }
+
+    if (exportQty > availableUnits)
+    {
+        std::cout << "\n[!] Not enough stock to export " << exportQty << " units.\n";
+        return false;
+    }
+
+    Item exportedItem = current->item;
+    exportedItem.units = exportQty;
+
+    current->item.units -= exportQty;
+
+    if (current->item.units == 0)
+    {
+        if (prev == nullptr)
+        {
+            ls->head = current->next;
+            if (current == ls->tail)
+                ls->tail = nullptr;
+        }
+        else
+        {
+            prev->next = current->next;
+            if (current == ls->tail)
+                ls->tail = prev;
+        }
+        delete current;
+        ls->n--;
+    }
+
+    name = exportedItem.name;
+    qtyOut = exportQty;
+
+    std::cout << "\n[-] Exported " << exportQty << " unit(s) of item name " << name << ".\n";
     return true;
 }
